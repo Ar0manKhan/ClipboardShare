e3f2d702aa66d0bef4385c51f721246a9113ce02
Ar0manKhan <73093255+Ar0manKhan@users.noreply.github.com>
2022-02-18 09:06:16 +0530

    Setup the view for creating the new room

--- basic/views.py
+++ basic/views.py
@@ -1,8 +1,31 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from .models import Room, Clipboard
+from django.views import View
+from django.db.utils import IntegrityError
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Just a test")
+def create(request):
+    room_code = None
+    # Run the loop untill the unqiue room_code is found.
+    # Also having loop limit of 10 runs.
+    for _ in range(10):
+        try:
+            room = Room()
+            room.save()
+            room_code = room.room_code
+            break
+        except IntegrityError:
+            pass
+    return redirect(reverse("basic:getroom", args=[room_code, ]))
+
+
+class GetRoom(View):
+    def get(self, request, room_id):
+        return HttpResponse(f"Your room id is {room_id}")
+
+    def post(self, request):
+        pass
