2f766101cff2da4cbafd99e53000f18571e4af3f
Ar0manKhan <73093255+Ar0manKhan@users.noreply.github.com>
2022-02-18 09:07:12 +0530

    setup urls.py for index view, room view

--- basic/urls.py
+++ basic/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
+app_name = "basic"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", TemplateView.as_view(template_name="basic/index.html")),
+    path("create", views.create, name="create"),
+    path("getroom/<slug:room_id>", views.GetRoom.as_view(), name="getroom")
 ]
